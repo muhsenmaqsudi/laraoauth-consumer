8b0e302cee08ec9c5709ea7dff28ec8ed17f384c
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-05-21 18:29:30 +0430

    added oauth state for preventing csrf attack

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,26 +14,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use Illuminate\Http\Request;
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
+    \Illuminate\Support\Facades\Cookie::queue('state', $state = \Illuminate\Support\Str::random(40), 1);
+
     $query = http_build_query([
-        'client_id' => env('LARAOAUTH_CLIENT_ID'), // Replace with Client ID
+        'client_id' => env('LARAOAUTH_CLIENT_ID'),
         'redirect_uri' => env('LARAOAUTH_REDIRECT_URI'),
         'response_type' => 'code',
-        'scope' => ''
+        'scope' => '',
+        'state' => $state
     ]);
 
     return redirect('http://127.0.0.1:8000/oauth/authorize?' . $query);
 });
 
+
+
 Route::get('/callback', function (Request $request) {
-    $response = (new GuzzleHttp\Client)->post('http://127.0.0.1:8000/oauth/token', [
+    $state = \Illuminate\Support\Facades\Cookie::get('state');
+
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class
+    );
+
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://127.0.0.1:8000/oauth/token', [
         'form_params' => [
             'grant_type' => 'authorization_code',
-            'client_id' => env('LARAOAUTH_CLIENT_ID'), // Replace with Client ID
-            'client_secret' => env('LARAOAUTH_CLIENT_SECRET'), // Replace with client secret
-            'redirect_uri' => 'http://127.0.0.1:8001/callback',
+            'client_id' => env('LARAOAUTH_CLIENT_ID'),
+            'client_secret' => env('LARAOAUTH_CLIENT_SECRET'),
+            'redirect_uri' => env('LARAOAUTH_REDIRECT_URI'),
             'code' => $request->code,
         ]
     ]);
